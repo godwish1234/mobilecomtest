7ae2d9cea921a7c766c02f3295f083ffb09bae47
godwish1234 <godwish_jakin@hotmail.com>
2022-01-26 10:08:02 +0700

    Initial Commit

---
+++ mobilecom-test/mobilecom_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobilecom_testApp.swift
+//  mobilecom-test
+//
+//  Created by Godwish Jakin on 26/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct mobilecom_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
